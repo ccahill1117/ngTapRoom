12f809264d3db7e85e78ddc06e08e7543e2c61b5
Christopher Cahill and Phil Mass <phil@>
2019-01-24 09:34:39 -0800

    root component html initialized and keg objects hardcoded

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Keg } from './models/keg.model'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  kegs: Keg[] = [
+    new Keg({name: "American Lager", brand: "Rainier", price: 125, pricePint: 3, alcoholContent: 4.60, ibu: 10}),
+    new Keg({name: "Red Chair NWPA", brand: "Deschutes", price: 150, pricePint: 5, alcoholContent: 6.20, ibu: 60}),
+    new Keg({name: "Drop Top Amber Ale", brand: "Widmer Brothers", price: 135, pricePint: 5, alcoholContent: 5.00, ibu: 20}),
+    new Keg({name: "O'Doul's", brand: "Anheuser-Busch", price: 100, pricePint: 2, alcoholContent: 0.50, ibu: 5})
+
+  ]
 }
